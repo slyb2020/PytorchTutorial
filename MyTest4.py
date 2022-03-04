499b1839d1290bbca293189b0f3558da1592ca38
slyb <slyb@tju.edu.cn>
2022-03-05 00:17:36 +0800

    2022年3月5日8:52在Surface上提交，下载了ResNet8

--- MyTest4.py
+++ MyTest4.py
@@ -3,4 +3,6 @@ import torchvision
 # entrypoints = torch.hub.list('pytorch/vision', force_reload=True)
 # print(torch.hub.help('pytorch/vision', 'resnet18', force_reload=True))
 
-torchvision.datasets.EMNIST("E:\\WorkSpace\\Dataset\\", split='mnist', download=True)
+# torchvision.datasets.EMNIST("E:\\WorkSpace\\Dataset\\", split='mnist', download=True)
+model = torchvision.models.resnet18(pretrained=True)
+print(model)
